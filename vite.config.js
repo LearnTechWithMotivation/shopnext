3b03a1ff7d0f5e46e8258f1a02bb10024d95f3d9
LearnTechWithMotivation <learntech466@example.com>
2025-04-23 11:00:05 +0530

    may work for Vercel

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base: '/shopnext/',
+  base: process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1'
+    ? '/shopnest-new/' // for GitHub Pages
+    : '/',  
 })
